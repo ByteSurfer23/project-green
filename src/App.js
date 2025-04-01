e5de80b127e248d49e41ecfbaba72a2dbc863715
Jabez Sam Sunder <jabezthedeveloper@gmail.com>
2025-04-01 13:22:13 +0530

    mroe minor changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import "./App.css";
 import Admin from "./main_pages/admin";
 import Home from "./main_pages/Home";
-import About from "./main_pages/About";
-import Search from "./main_pages/Search";
-import GreenCover from "./main_pages/GreenCover";
 import SearchPage from "./main_pages/SearchPage";
 
 function App() {
   return (
     <>
-      <Router>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/search">Search</Link>
-            </li>
-          </ul>
-        </nav> */}
+    <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/search" element={<SearchPage/>} />
+          <Route path="/search" element={<SearchPage />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
